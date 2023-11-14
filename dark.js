488d5f21f48900abfc0eab39ae7f1f1ac41faa79
frankellysanchez <frankellysanchez1202@gmail.com>
2023-11-14 18:13:25 -0400

    upgraded

--- dark.js
+++ dark.js
@@ -8,32 +8,22 @@ export default class Dack {
         const navLogo = document.querySelector(".nav-logo");
         const subTittle = document.getElementById("subTittle");
         const copyright = document.querySelector(".copyright");
-        const navLink = document.querySelector(".nav-link1")
-        const navLink2 = document.querySelector(".nav-link2")
-        const navLink3 = document.querySelector(".nav-link3")
-        const navLink4 = document.querySelector(".nav-link4");
         const search_bar = document.getElementById("search_bar");
-        const day =  document.querySelector(".day");
-        const night =document.querySelector(".night");
         const navImmages = document.querySelector(".nav_images");
-        const showMovieInfContainer = document.querySelector(".showMovieInfContainer");
+        const navBurger = document.querySelector(".nav-burger");
+        const day = document.querySelector(".day");
         
         darkBtn.addEventListener('click', () => { 
+            day.classList.toggle("dayColor");
+            navBurger.classList.toggle("navBurgerColor");
             body.classList.toggle("bodyColor");
             footercontainer.classList.toggle("footer_containerColor");
             hero.classList.toggle("heroColor");
             navLogo.classList.toggle("nav-logoColor")
             subTittle.classList.toggle("subTittleColor")
             copyright.classList.toggle("copyrightColor")
-            navLink.classList.toggle("nav-linkColor");
-            navLink2.classList.toggle("nav-linkColor");
-            navLink3.classList.toggle("nav-linkColor");
-            navLink4.classList.toggle("nav-linkColor");
             search_bar.classList.toggle("search_barColor");
-            darkBtn.classList.toggle("btn-dark");
-            day.classList.toggle("day2");
             navImmages.classList.toggle("navBurgerColor")
-            showMovieInfContainer.classList.toggle("showMovieInfContainerColor")
         })
         
     }
